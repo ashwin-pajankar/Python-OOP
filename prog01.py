bc466daba9e6ef2a1ea41296ccc917500c33cb00
ASHWIN PAJANKAR <ashwin-pajankar@users.noreply.github.com>
2017-07-31 21:12:48 +0530

    Added 2 new Python Programs
    Added 2 new Python Programs

---
+++ prog01.py
@@ -0,0 +1,10 @@
+class Point:
+    pass
+
+
+p1 = Point()
+p2 = Point()
+
+
+print(p1)
+print(p2)
---
+++ prog02.py
@@ -0,0 +1,19 @@
+class Point:
+    pass
+
+
+p1 = Point()
+
+p1.x = 2
+p1.y = 3
+p1.z = 5
+
+print(p1.x, p1.y, p1.z)
+
+p2 = Point()
+
+p2.x = 6
+p2.y = 2
+p2.z = -4
+
+print(p1.x, p1.y, p1.z)
